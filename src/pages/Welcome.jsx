16fb354e3f62d99951ebe744421893a01086694f
SAMUEL WILLIAMS <108836524+sammelo2000@users.noreply.github.com>
2023-11-06 01:39:10 +0200

    Welocomd Header

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,33 +1,12 @@
 // import SmileyOneIcon from '../assets/svg/smileyOneIcon.svg?react';
 // import SmileyTwoIcon from '../assets/svg/smileyTwoIcon.svg?react';
 // import SmileyThreeIcon from '../assets/svg/smileyThreeIcon.svg?react';
-import WelcomePicIcon from '../assets/svg/welcomePicIcon.svg?react';
-import FarmHorizonIcon from '../assets/svg/farmHorizonIcon.svg?react';
+import { WelcomeHeader } from "../components/WelcomeHeader";
 
 const Welcome = () => {
   return (
-    <div className="relative">
-      <h1 className="">Welcome to Smart Farm</h1>
-      <h4 className="">Let's get started. Tell us about your farm and preferences</h4>
-
-      <div className="flex">
-      <div>
-          {/* <SmileyOneIcon /> */}
-        </div>
-        <div>
-          {/* <SmileyTwoIcon /> */}
-        </div>
-        <div>
-          {/* <SmileyThreeIcon /> */}
-        </div>
-      </div>
-      
-      <div className="absolute top-0 left-0">
-        <FarmHorizonIcon className="z-10" />
-        <WelcomePicIcon className="z-1" />
-      </div>
-    </div>
-  )
+   <WelcomeHeader />
+  );
 }
 
 export default Welcome;
