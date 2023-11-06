f6de3f1e667aa2768db4a9fbb95bc86a5b7118c5
SAMUEL WILLIAMS <108836524+sammelo2000@users.noreply.github.com>
2023-11-07 01:44:04 +0200

    tonights changes

--- src/pages/AddCrop.jsx
+++ src/pages/AddCrop.jsx
@@ -1,16 +1,10 @@
-import { useState, useEffect } from "react"
-import { db, auth } from "../config/firebase"
-import { Auth } from "../components/auth"
-import Sidebar from "../components/Sidebar"
-import { useNavigate } from "react-router-dom"
-
-
-import {
-  getDocs,
-  collection,
-  addDoc,
-} from "firebase/firestore"
-import Logo from "../assets/svg/logo.svg"
+import { useState, useEffect } from "react";
+import { db, auth } from "../config/firebase";
+import { Auth } from "../components/auth";
+import Sidebar from "../components/Sidebar";
+import { useNavigate } from "react-router-dom";
+import { getDocs, collection, addDoc } from "firebase/firestore";
+import Logo from "../assets/svg/logo.svg";
 
 const AddCrop = () => {
   const navigate = useNavigate();
@@ -35,8 +29,6 @@ const AddCrop = () => {
 
   const cropsCollectionRef = collection(db, "crops");
 
-
-
   const getCropList = async () => {
     try {
       const data = await getDocs(cropsCollectionRef);
@@ -44,41 +36,39 @@ const AddCrop = () => {
         ...doc.data(),
         id: doc.id,
       }));
-      console.log(filteredData);
       setCropList(filteredData);
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
   useEffect(() => {
     getCropList();
   }, []);
 
   const onSubmitCrop = async (e) => {
-    e.preventDefault(); // Prevent the form from submitting and refreshing the page
+    e.preventDefault();
 
     try {
       await addDoc(cropsCollectionRef, {
         type: newCropType,
         number: newCropNumber,
         avgSize: newCropSize,
-        area: newArea, // Add area
-        plantingMethod: newPlantingMethod, // Add plantingMethod
-        plantingDate: newPlantingDate, // Add plantingDate
-        estimatedHarvestDate: newEstimatedHarvestDate, // Add estimatedHarvestDate
-        growthStage: newGrowthStage, // Add growthStage
-        status: newStatus, // Add status
-        fertilizationDate: newFertilizationDate, // Add fertilizationDate
-        soilType: newSoilType, // Add soilType
-        soilPH: newSoilPH, // Add soilPH
-        pestControl: newPestControl, // Add pestControl
-        actualHarvestDate: newActualHarvestDate, // Add actualHarvestDate
-        lastUpdate: newLastUpdate, // Add lastUpdate
-
+        area: newArea,
+        plantingMethod: newPlantingMethod,
+        plantingDate: newPlantingDate,
+        estimatedHarvestDate: newEstimatedHarvestDate,
+        growthStage: newGrowthStage,
+        status: newStatus,
+        fertilizationDate: newFertilizationDate,
+        soilType: newSoilType,
+        soilPH: newSoilPH,
+        pestControl: newPestControl,
+        actualHarvestDate: newActualHarvestDate,
+        lastUpdate: newLastUpdate,
         userId: auth?.currentUser?.uid,
       });
-      // Clear input fields
+
       setNewCropType("");
       setNewCropNumber("");
       setNewCropSize("");
@@ -100,160 +90,173 @@ const AddCrop = () => {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
     <>
-      <div className="logo">
+    <div className="logo">
         <img src={Logo} alt="Your Logo" width="500px" />
       </div>
       <div className="flex">
         <Sidebar />
-        <div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Crop type"
-              value={newCropType}
-              onChange={(e) => setNewCropType(e.target.value)}
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Number"
-              type="text"
-              value={newCropNumber}
-              onChange={(e) => setNewCropNumber(e.target.value)}
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Average Size"
-              type="text"
-              value={newCropSize}
-              onChange={(e) => setNewCropSize(e.target.value)}
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Area" // Add area
-              type="text" // Update the input type if needed
-              value={newArea} // Use the state variable
-              onChange={(e) => setNewArea(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Planting Method" // Add plantingMethod
-              type="text" // Update the input type if needed
-              value={newPlantingMethod} // Use the state variable
-              onChange={(e) => setNewPlantingMethod(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Planting Date" // Add plantingDate
-              type="text" // Update the input type if needed
-              value={newPlantingDate} // Use the state variable
-              onChange={(e) => setNewPlantingDate(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Estimated Harvest Date" // Add estimatedHarvestDate
-              type="text" // Update the input type if needed
-              value={newEstimatedHarvestDate} // Use the state variable
-              onChange={(e) => setNewEstimatedHarvestDate(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Growth Stage" // Add growthStage
-              type="text" // Update the input type if needed
-              value={newGrowthStage} // Use the state variable
-              onChange={(e) => setNewGrowthStage(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Status" // Add status
-              type="text" // Update the input type if needed
-              value={newStatus} // Use the state variable
-              onChange={(e) => setNewStatus(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Fertilization Date" // Add fertilizationDate
-              type="text" // Update the input type if needed
-              value={newFertilizationDate} // Use the state variable
-              onChange={(e) => setNewFertilizationDate(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Soil Type" // Add soilType
-              type="text" // Update the input type if needed
-              value={newSoilType} // Use the state variable
-              onChange={(e) => setNewSoilType(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Soil pH" // Add soilPH
-              type="text" // Update the input type if needed
-              value={newSoilPH} // Use the state variable
-              onChange={(e) => setNewSoilPH(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Pest Control" // Add pestControl
-              type="text" // Update the input type if needed
-              value={newPestControl} // Use the state variable
-              onChange={(e) => setNewPestControl(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Actual Harvest Date" // Add actualHarvestDate
-              type="text" // Update the input type if needed
-              value={newActualHarvestDate} // Use the state variable
-              onChange={(e) => setNewActualHarvestDate(e.target.value)} // Set the state
-            />
-          </div>
-          <div className="mb-2">
-            <input
-              className="p-2 border border-gray-300 rounded"
-              placeholder="Last Update" // Add lastUpdate
-              type="text" // Update the input type if needed
-              value={newLastUpdate} // Use the state variable
-              onChange={(e) => setNewLastUpdate(e.target.value)} // Set the state
-            />
-          </div>
-          <button
-            className="p-2 bg-green-700 text-white rounded hover:bg-green-800"
-            onClick={onSubmitCrop}
+        <div className="w-full max-w-md mx-auto">
+          {/* <h1 className="text-green">Add Crop</h1> */}
+          <form
+            onSubmit={onSubmitCrop}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
           >
-            Add Crop
-          </button>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Crop Type"
+                type="text"
+                value={newCropType}
+                onChange={(e) => setNewCropType(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Number"
+                type="text"
+                value={newCropNumber}
+                onChange={(e) => setNewCropNumber(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Average Size"
+                type="text"
+                value={newCropSize}
+                onChange={(e) => setNewCropSize(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Area"
+                type="text"
+                value={newArea}
+                onChange={(e) => setNewArea(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Planting Method"
+                type="text"
+                value={newPlantingMethod}
+                onChange={(e) => setNewPlantingMethod(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Planting Date"
+                type="text"
+                value={newPlantingDate}
+                onChange={(e) => setNewPlantingDate(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Estimated Harvest Date"
+                type="text"
+                value={newEstimatedHarvestDate}
+                onChange={(e) =>
+                  setNewEstimatedHarvestDate(e.target.value)
+                }
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Growth Stage"
+                type="text"
+                value={newGrowthStage}
+                onChange={(e) => setNewGrowthStage(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Status"
+                type="text"
+                value={newStatus}
+                onChange={(e) => setNewStatus(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Fertilization Date"
+                type="text"
+                value={newFertilizationDate}
+                onChange={(e) => setNewFertilizationDate(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Soil Type"
+                type="text"
+                value={newSoilType}
+                onChange={(e) => setNewSoilType(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Soil pH"
+                type="text"
+                value={newSoilPH}
+                onChange={(e) => setNewSoilPH(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Pest Control"
+                type="text"
+                value={newPestControl}
+                onChange={(e) => setNewPestControl(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Actual Harvest Date"
+                type="text"
+                value={newActualHarvestDate}
+                onChange={(e) =>
+                  setNewActualHarvestDate(e.target.value)
+                }
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Last Update"
+                type="text"
+                value={newLastUpdate}
+                onChange={(e) => setNewLastUpdate(e.target.value)}
+              />
+            </div>
+            <div className="text-center">
+              <button
+                className="p-2 bg-green-700 text-white rounded hover:bg-green-800"
+                onClick={onSubmitCrop}
+              >
+                Add Crop
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default AddCrop;
