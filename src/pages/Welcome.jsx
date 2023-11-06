847f7c7599a3cf4aaa6f9abcd968099114711985
SAMUEL WILLIAMS <108836524+sammelo2000@users.noreply.github.com>
2023-11-06 19:38:11 +0200

    Welcome page skelethon

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,15 +2,19 @@
 // import SmileyTwoIcon from '../assets/svg/smileyTwoIcon.svg?react';
 // import SmileyThreeIcon from '../assets/svg/smileyThreeIcon.svg?react';
 import { WelcomeAlmostThere } from "../components/WelcomeAlmostThere";
+import { WelcomeCompleted } from "../components/WelcomeCompleted";
 import { WelcomeGetStarted } from "../components/WelcomeGetStarted";
 import { WelcomeHeader } from "../components/WelcomeHeader";
 
+
 const Welcome = () => {
+  
   return (
    <div className="welcomePageParent">
     <WelcomeHeader />
-    {/* <WelcomeGetStarted /> */}
-    <WelcomeAlmostThere />
+    <WelcomeGetStarted />
+    {/* <WelcomeAlmostThere /> */}
+    {/* <WelcomeCompleted /> */}
    </div>
     
    
