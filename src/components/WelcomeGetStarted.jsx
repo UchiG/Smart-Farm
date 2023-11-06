ab4c676bbdbc5404e08dc86fe29e0c4308bbd72a
SAMUEL WILLIAMS <108836524+sammelo2000@users.noreply.github.com>
2023-11-06 03:31:03 +0200

    getting started page done

--- src/components/WelcomeGetStarted.jsx
+++ src/components/WelcomeGetStarted.jsx
@@ -2,7 +2,30 @@ export const WelcomeGetStarted = () => {
 
     return (
             <div className="welcomeGetStartedParent">
-                <div className="welcomeGetStaredChild getStartedFormParent">ok</div>
+                <div className="welcomeGetStaredChild getStartedFormParent">
+                    <form action="">
+                        <div className="welcomeGetStartedInputField">
+                            <label htmlFor="farmName" className="welcomeGetStartedInputLabe">Farm Name</label>
+                            <input type="text" className="welcomeGetStartedInput" />
+                        </div>
+                        <div className="welcomeGetStartedInputField">
+                            <label htmlFor="address" className="welcomeGetStartedInputLabe">Address</label>
+                            <input type="text" className="welcomeGetStartedInput"/>
+                        </div>
+                        <div className="welcomeGetStartedInputField">
+                            <label htmlFor="country" className="welcomeGetStartedInputLabe">Country</label>
+                            <select name="" id="" className="welcomeGetStartedInput"></select>
+                        </div>
+                        <div className="welcomeGetStartedInputField">
+                            <label htmlFor="district" className="welcomeGetStartedInputLabe">District</label>
+                            <input type="text" className="welcomeGetStartedInput"/>
+                        </div>
+                        <div className="welcomeGetStartedInputField">
+                            <label htmlFor="city" className="welcomeGetStartedInputLabe">City</label>
+                            <input type="text"className="welcomeGetStartedInput"/>
+                        </div>
+                    </form>
+                </div>
                 <div className="welcomeGetStaredChild getStartedImage"></div>
             </div>
     
